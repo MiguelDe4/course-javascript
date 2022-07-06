e7a6bede547ec6bd9f7f6e371f35b5b7f8947ae0
Михаил Усюкин <MiguelUs@yandex.ru>
2022-07-06 21:31:28 +0300

    Выполнил шестое ДЗ

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,13 +19,62 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
+// const div = document.querySelector('.draggable-div');
+
 document.addEventListener('mousemove', (e) => {});
 
-export function createDiv() {}
+function addListeners(target) {
+  let shiftX, shiftY;
+
+  function getCoords(elem) {
+    const box = elem.getBoundingClientRect();
+
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset,
+    };
+  }
+
+  function handleDragStart(e) {
+    shiftX = e.pageX - getCoords(target).left;
+    shiftY = e.pageY - getCoords(target).top;
+  }
+
+  function handleDragEnd(e) {
+    target.style.left = e.pageX - shiftX + 'px';
+    target.style.top = e.pageY - shiftY + 'px';
+  }
+
+  target.addEventListener('dragend', handleDragEnd);
+  target.addEventListener('dragstart', handleDragStart);
+}
+
+export function createDiv() {
+  const div = document.createElement('div');
+  div.className = 'draggable-div';
+  div.draggable = true;
+  div.style.height = getRandomSize();
+  div.style.width = getRandomSize();
+  div.style.top = getRandomSize();
+  div.style.left = getRandomSize();
+  div.style.backgroundColor = `rgb(${getRandomColor(0, 255)}, ${getRandomColor(
+    0,
+    255
+  )}, ${getRandomColor(0, 255)})`;
+  div.style.position = 'absolute';
+  div.style.cursor = 'pointer';
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
+
+const getRandomSize = () => Math.floor(Math.random() * 500) + 'px';
+function getRandomColor(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
+}
 
 addDivButton.addEventListener('click', function () {
   const div = createDiv();
   homeworkContainer.appendChild(div);
+  addListeners(div);
 });
