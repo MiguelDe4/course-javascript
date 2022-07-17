7735947d618581c29639ebc71ea47cea70d1d0d7
Михаил Усюкин <MiguelUs@yandex.ru>
2022-07-17 08:50:31 +0300

    Выполнил первое выпускное задание

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,51 @@
+const webpack = require('webpack');
 const HtmlPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const fs = require('fs');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const rules = require('./webpack.config.rules')();
 const path = require('path');
 
-const root = path.resolve('projects');
-const projects = fs.readdirSync(root);
-const entries = {};
-const htmlPlugins = [];
-const proxy = {};
-
-for (const project of projects) {
-  const projectPath = path.join(root, project);
-
-  entries[project] = projectPath;
-  htmlPlugins.push(
-    new HtmlPlugin({
-      title: project,
-      template: path.resolve('./layout.html'),
-      filename: `${project}/index.html`,
-      chunks: [project],
+rules.push({
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
     })
-  );
-
-  const settingsPath = path.join(projectPath, 'settings.json');
-
-  if (fs.existsSync(settingsPath)) {
-    const settings = require(settingsPath);
-    Object.assign(proxy, settings.proxy);
-  }
-}
-
-const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+});
 
 module.exports = {
-  entry: entries,
-  output: {
-    filename: mode === 'production' ? 'name/[chunkhash].js' : '[name]/[name].js',
-    path: path.resolve('dist'),
-  },
-  mode,
-  devServer: {
-    proxy,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: { cacheDirectory: true },
-      },
-      {
-        test: /\.html/,
-        include: [path.resolve(__dirname, 'projects')],
-        use: [
-          { loader: './scripts/html-inject-loader.js' },
-          {
-            loader: 'raw-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg|eot|ttf|woff|woff2)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[hash:8].[ext]',
-          outputPath: 'reosurces',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-    ...htmlPlugins,
-    new CleanWebpackPlugin(),
-  ],
+    entry: path.resolve(__dirname, 'projects') + '/src/index.js',
+    devServer: {
+        index: 'index.html',
+        overlay: true
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve('dist')
+    },
+    devtool: 'source-map',
+    module: { rules },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    compress: false,
+                    ecma: 6,
+                    mangle: true
+                },
+                sourceMap: true
+            })
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new HtmlPlugin({
+            title: 'GeoFeedBack',
+            template: 'index.hbs'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
 };
